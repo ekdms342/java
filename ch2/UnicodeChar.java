71aa865f8d8161952c2fe102c1e7761dea7e595f
ekdms342 <ekdms342@gmail.com>
2020-04-14 15:09:34 +0900

    2020.04.14

---
+++ ch2/UnicodeChar.java
@@ -0,0 +1,15 @@
+class UnicodeChar
+{
+    public static void main(String[] args)
+    {
+        char c1 ='A';
+        char c2 = '한';
+        char c3 = 0x3091;
+        char c4 = 0x3092;
+
+        System.out.println(c1); 
+        System.out.println(c2); 
+        System.out.println(c3); 
+        System.out.println(c4); 
+    }
+}
---
+++ ch2/VariableDecl.java
@@ -0,0 +1,13 @@
+class VariableDecl
+{
+    public static void main (String[]args)
+    {
+        double num1,num2,result;
+        num1 =1.000001;
+        num2 = 2.000001;
+        result = num1 +num2;
+
+        System.out.println(result);
+
+    }
+}
