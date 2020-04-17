2922d8c967dfd63d0ea12d70d0472f55a0871c15
ekdms342 <ekdms342@gmail.com>
2020-04-17 15:02:45 +0900

    2020.04.17

--- ch6/ch6_3_Q2.java
+++ ch6/ch6_3_Q2.java
@@ -1,18 +1,12 @@
-
 class ch6_3_Q2 {
     public static void main(String[]args)
     {
-        int num = 12;
-        TenToTwo(num);
-    } 
-    public static void TenToTwo(int num)
-    {
-        if(num == 0)
-        {
-            return;    
-        }
-        TenToTwo(num/2);
-        System.out.println (num % 2);
+        int num = 4;
+        System.out.println(TenToTwo(num));
         
+    } 
+    public static String TenToTwo(int num)
+    {
+        return (num == 0) ? "" : TenToTwo(num/2)+(num % 2);
     }
 }
